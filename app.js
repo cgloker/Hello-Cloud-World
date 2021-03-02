dd4bc8218e2bab7d4bbd66da204dc2f2eab561e5
Tuyet <toowee.le@gmail.com>
2021-03-02 12:18:52 +0000

    update file structure

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 3000
 
+app.use(express.static('public')) // tells express ot look at everything in the public folder
+
+// renders the index.html (homepage)
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+  //res.send('Hello World!')
 })
 
 app.listen(port, () => {
